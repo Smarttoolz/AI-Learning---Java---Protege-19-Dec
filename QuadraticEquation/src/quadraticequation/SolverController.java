5c68903bbe50f431f2b27e9cd1f043f1a9b250d7
Adefuwa Oluwaponmile Hussein <ogunyemioluwafemi@gmail.com>
2024-12-18 15:52:44 +0100

    Create SolverController.java

---
+++ QuadraticEquation/src/quadraticequation/SolverController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package quadraticequation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Home
+ */
+public class SolverController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void home(ActionEvent event) {
+    }
+
+    @FXML
+    private void learning(ActionEvent event) {
+    }
+
+    @FXML
+    private void calculator(ActionEvent event) {
+    }
+
+    @FXML
+    private void quit(ActionEvent event) {
+    }
+    
+    public static void display() {
+      
+    }
+    
+}
